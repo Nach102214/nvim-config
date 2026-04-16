69950b05d29401244676ec01d2d8ff137a937bd3
Nach102214 <nach10@mail.ru>
2026-04-16 17:20:50 +0900

    At the end of the file, added autoformatting of the code text according to PEP8

--- init.lua
+++ init.lua
@@ -209,3 +209,12 @@ if ok_dap then
         dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
     end
 end
+
+-- Автоформатирование при сохранении
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.py",
+    callback = function()
+        vim.lsp.buf.format({ async = false })
+    end,
+})
+
